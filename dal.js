b4a0e38396de1ce5ad802064c075b4d26312adf6
Nicholas D Vaughan <127707329+ndvaughan@users.noreply.github.com>
2024-03-12 18:43:45 -0700

    Update dal.js
    updated dal.js based on error message received re:db

--- dal.js
+++ dal.js
@@ -1,80 +1,59 @@
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb+srv://ndvaughan1:Z30s1VfqMcEl2c5N@cluster0.2jvys0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster';
+const url = 'mongodb+srv://ndvaughan1:Z30s1VfqMcEl2c5N@cluster0.2jvys0u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
 let db = null;
 
 // connect to mongo
-MongoClient.connect(url, { useUnifiedTopology: true }, function (err, client) {
-    console.log("Connected successfully to db server");
+MongoClient.connect(url, function (err, client) {
+    if (err) {
+        console.error("Failed to connect to the database:", err);
+        return;
+    }
 
-    // connect to myproject database
+    console.log("Connected successfully to db server");
     db = client.db('myproject');
 });
 
 // create user account using the collection.insertOne function
-function create(name, email, password) {
-    // TODO: populate this function based off the video
-    return new Promise((resolve, reject) => {
-        const collection = db.collection('users');
-        const doc = {name, email, password, balance: 0};
-        collection.insertOne(doc, {w:1}, function(err, result) {
-            err ? reject(err) : resolve(doc);
-        });
-    })
+async function create(name, email, password) {
+    await connectDB();
+    const collection = db.collection('users');
+    const doc = { name, email, password, balance: 0 };
+    return collection.insertOne(doc);
 }
 
 // find user account 
-function find(email) {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .find({ email: email })
-            .toArray(function (err, docs) {
-                err ? reject(err) : resolve(docs);
-            });
-    })
+async function find(email) {
+    await connectDB();
+    return db.collection('users').find({ email: email }).toArray();
 }
 
 // find user account
-function findOne(email) {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .findOne({ email: email })
-            .then((doc) => resolve(doc))
-            .catch((err) => reject(err));
-    })
+async function findOne(email) {
+    await connectDB();
+    return db.collection('users').findOne({ email: email });
 }
 
 // update - deposit/withdraw amount
-function update(email, amount) {
-    return new Promise((resolve, reject) => {
-        const customers = db
-            .collection('users')
-            .findOneAndUpdate(
-                { email: email },
-                { $inc: { balance: amount } },
-                { returnOriginal: false },
-                function (err, documents) {
-                    err ? reject(err) : resolve(documents);
-                }
-            );
-
-
-    });
+async function update(email, amount) {
+    await connectDB();
+    return db.collection('users').findOneAndUpdate(
+        { email: email },
+        { $inc: { balance: amount } },
+        { returnOriginal: false }
+    );
 }
 
 // return all users by using the collection.find method
-function all() {
-    // TODO: populate this function based off the video
-    return new Promise((resolve, reject) => {
-        const custoemers = db
-        .collection('users')
-        .find({})
-        .toArray(function(err, docs) {
-            err ? reject(err) : resolve(docs);
-        });
-    })
+async function all() {
+    await connectDB();
+    return db.collection('users').find({}).toArray();
 }
 
+async function connectDB() {
+    if (db === null) {
+        throw new Error("Database connection has not been established yet.");
+    }
+}
 
 module.exports = { create, findOne, find, update, all };
